92200273dbff72a267fd0882cf7de5ff4999e544
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-09 09:58:44 -0400

    Create Ex6.java

---
+++ Ex6.java
@@ -0,0 +1,38 @@
+/* Make a program that reads three double-precision floating-point values: A, B, and C. Then calculates and show:
+   a) the area of the right-angled triangle that has A for base and C for height.
+   b) the area of the circle of radius C. (pi = 3.14159)
+   c) the area of the trapezoid that has A and B for bases and C for height.
+   d) the area of the square that has side B.
+   e) the area of the rectangle that has sides A and B. */
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, C, triangle, circle, trapezoid, square, rectangle;
+
+        System.out.print("Insert the value of A: ");
+        A = sc.nextDouble();
+        System.out.print("Insert the value of B: ");
+        B = sc.nextDouble();
+        System.out.print("Insert the value of C: ");
+        C = sc.nextDouble();
+
+        triangle = A * C / 2;
+        circle = 3.14159 * Math.pow(C, 2);
+        trapezoid = (A + B) / 2 * C;
+        square = Math.pow(B, 2);
+        rectangle = A * B;
+
+        System.out.printf("TRIANGLE: %.3f%n", triangle);
+        System.out.printf("CIRCLE: %.3f%n", circle);
+        System.out.printf("TRAPEZOID: %.3f%n", trapezoid);
+        System.out.printf("SQUARE: %.3f%n", square);
+        System.out.printf("RECTANGLE: %.3f%n", rectangle);
+
+        sc.close();
+    }
+}
