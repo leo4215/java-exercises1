961c88814669c85570d50dff050f3cc566d15cfc
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-09 09:16:49 -0400

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,19 @@
+/*Make a program to read two integer values, and then show on the screen the sum of those numbers with a explanatory message, as examples. */
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A, B, sum;
+    System.out.println("Insert the value of A:");
+		A = sc.nextInt();
+    System.out.println("Insert the value of B:");
+		B = sc.nextInt();
+
+		sum = A + B;
+		System.out.println("SUM = " + sum);
+		sc.close();
+	}
+}
