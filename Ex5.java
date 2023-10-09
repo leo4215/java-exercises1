c9606fd57af1e9300b3809c6bfcaeba5bd734964
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-09 09:50:02 -0400

    Create Ex5.java

---
+++ Ex5.java
@@ -0,0 +1,33 @@
+/* Make a program to read the code of a tool 1, the number of tools 1, the unit value of each tool 1, the code of tool 2, the number of tool 2, and the unit value of each tool 2. Calculate and show the amount to be paid. */
+
+import java.util.Scanner;
+
+public class Ex5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cod1, cod2, amount1, amount2;
+        double price1, price2, total;
+
+        System.out.print("Insert the code of the 1st tool: ");
+        cod1 = sc.nextInt();
+        System.out.print("Insert the amount of the 1st tool: ");
+        amount1 = sc.nextInt();
+        System.out.print("Insert the price of the 1st tool: ");
+        price1 = sc.nextDouble();
+
+        System.out.print("Insert the code of the 2nd tool: ");
+        cod2 = sc.nextInt();
+        System.out.print("Insert the amount of the 2nd tool: ");
+        amount2 = sc.nextInt();
+        System.out.print("Insert the price of the 2nd tool: ");
+        price2 = sc.nextDouble();
+
+        total = price1 * amount1 + price2 * amount2;
+
+        System.out.printf("TOTAL: $ %.2f%n", total);
+
+        sc.close();
+    }
+}
