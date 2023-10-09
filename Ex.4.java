da69264d9907cd5239e30ad47cc818bef672d376
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-09 09:39:20 -0400

    Create Ex.4.java

---
+++ Ex.4.java
@@ -0,0 +1,27 @@
+/* Make a program that reads an employee's ID number, their number of hours worked, the amount they get paid per hour, and calculates that employee's salary. Next, show the employee's ID and salary, with two decimal places. */
+
+import java.util.Scanner;
+
+public class Ex4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int ID, hours;
+        double wage, salary;
+
+        System.out.print("Insert the employee ID: ");
+        ID = sc.nextInt();
+        System.out.print("Insert the amount of working hours: ");
+        hours = sc.nextInt();
+        System.out.print("Insert the employee wage: ");
+        wage = sc.nextDouble();
+
+        salary = wage * hours;
+
+        System.out.println("ID = " + ID);
+        System.out.printf("SALARY = U$ %.2f%n", salary);
+
+        sc.close();
+    }
+}
