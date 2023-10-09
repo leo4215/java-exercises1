c7b92a559703a54d4773795f072e2373b7257bb2
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-09 09:24:44 -0400

    Create Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,21 +1,22 @@
-/*Make a program to read the value of the radius of a circle, and then show the value of the area of this circle with four decimal places as per examples.
-Area formula: area = π . radius^2
-Consider the value of π = 3.14159*/
+/* Make a program to read the value of the radius of a circle, and then show the value of the area of this circle with four decimal places as per examples.
+   Area formula: area = π . radius^2
+   Consider the value of π = 3.14159 */
 
 import java.util.Scanner;
 
 public class Ex2 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		double R, A, pi = 3.14159;
-	    System.out.println("Insert the value of the radius");
-	    R = sc.nextDouble();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double R, A, pi = 3.14159;
 
-	    A = pi * R * R;
+        System.out.println("Insert the value of the radius");
+        R = sc.nextDouble();
 
-	    System.out.printf("A=%.4f%n", A);
+        A = pi * Math.pow(R, 2);
 
-		sc.close();
-	}
+        System.out.printf("A=%.4f%n", A);
+
+        sc.close();
+    }
 }
