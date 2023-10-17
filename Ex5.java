2fde301dcc56c4751cabc94ad29ad31a4f571243
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-17 11:00:46 -0400

    Create Ex5.java

--- Ex5.java
+++ Ex5.java
@@ -10,18 +10,18 @@ public class Ex5 {
         int cod1, cod2, amount1, amount2;
         double price1, price2, total;
 
-        System.out.print("Insert the code of the 1st tool: ");
+        System.out.print("Enter the code of the 1st tool: ");
         cod1 = sc.nextInt();
-        System.out.print("Insert the amount of the 1st tool: ");
+        System.out.print("Enter the amount of the 1st tool: ");
         amount1 = sc.nextInt();
-        System.out.print("Insert the price of the 1st tool: ");
+        System.out.print("Enter the price of the 1st tool: ");
         price1 = sc.nextDouble();
 
-        System.out.print("Insert the code of the 2nd tool: ");
+        System.out.print("Enter the code of the 2nd tool: ");
         cod2 = sc.nextInt();
-        System.out.print("Insert the amount of the 2nd tool: ");
+        System.out.print("Enter the amount of the 2nd tool: ");
         amount2 = sc.nextInt();
-        System.out.print("Insert the price of the 2nd tool: ");
+        System.out.print("Enter the price of the 2nd tool: ");
         price2 = sc.nextDouble();
 
         total = price1 * amount1 + price2 * amount2;
